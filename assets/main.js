5da8d20b163e7ac84584bb48c92f8f5877010026
Ben Longden <ben.longden@theguardian.com>
2021-02-05 21:08:25 -0300

    added structure

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+console.log('running');
+
+function menu(){
+  var freelance = document.querySelector('.umbrella_freelance');
+  var client = document.querySelector('.umbrella_clients');
+  var menuParent = document.querySelector('.umbrella_menu');
+  var body = document.querySelector('body');
+  console.log(freelance);
+  freelance.addEventListener('click', function(){
+    this.classList.toggle('open');
+    menuParent.classList.toggle('open');
+    body.classList.toggle('open');
+  });
+  client.addEventListener('click', function(){
+    this.classList.toggle('open');
+    menuParent.classList.toggle('open');
+    body.classList.toggle('open');
+  });
+}menu();
--- freelance.markdown
+++ freelance.markdown
@@ -1,5 +1,5 @@
 ---
 title: Freelance
 date: 2021-02-05 23:21:00 Z
+layout: freelance
 ---
-
